0a169536e002c5747b447109f08a9109ba919bb3
Sergey S. Volkov <js2me@outlook.com>
2020-02-12 15:06:56 +0300

    Create ru.js

---
+++ app/client/locales/ru.js
@@ -0,0 +1,46 @@
+export default {
+  atoms: {
+    reset: 'Сброс',
+    selectFile: 'Файл',
+    selectDir: 'Папка'
+  },
+  wads: {
+    oldest: 'Сначала старые',
+    newest: 'Сначала новые',
+    filter: 'Фильтр {{size}} режимов',
+    toastIndex: 'Reindexed Mod Directory'
+  },
+  sourceports: {
+    delete: 'Удалить порт',
+    save: 'Сохранить порт',
+    binary: 'Путь к порту',
+    name: 'Название порта',
+    savegameparam: 'Использовать сохранение',
+    configparam: 'Использовать конфиг',
+    screenshot: 'Использовать Скриншоты',
+    toastSaved: 'Порт успешно сохранен',
+    toastDeleted: 'Порт удален'
+  },
+  settings: {
+    savepath: 'Savegame Directory',
+    screenpath: 'Screenshot Directory',
+    waddir: 'WAD Directory',
+    wallpaper: 'Wallpaper Image',
+    save: 'Save Settings',
+    toastSaved: 'Successfully saved Settings'
+  },
+  common: {
+    loading: 'Загрузка',
+    language: 'Язык',
+    success: 'Успешно',
+    cancel: 'Отмена',
+    ok: 'Ок'
+  },
+  nav: {
+    appname: 'Super Shotgun Launcher',
+    wads: 'Wads',
+    packages: 'Пакеты',
+    sourceports: 'Порты',
+    settings: 'Настройки'
+  }
+};
